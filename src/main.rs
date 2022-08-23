bab127712b7058cb15f17483b79b1a8a9982ad9e
Renkai Ge <renkaige@tubi.tv>
2022-08-23 16:17:35 +0800

    cargo fix

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Args {
     method: Method,
 
     #[clap(value_parser)]
-    file_path: String
+    file_path: String,
 }
 
 fn main() {
@@ -39,14 +39,14 @@ fn main() {
             let mut buf = Vec::new();
             meta_pb.encode(&mut buf).unwrap();
             let decoded = MetaPB::decode(&*buf);
-            println!("decoded: {:?}",decoded);
+            println!("decoded: {:?}", decoded);
             use lance_tools::format::metadata::get_schema;
 
             println!("path: {:?}", args.file_path);
             let mut file = fs::File::open(args.file_path).unwrap();
 
-            let metadata = get_schema(&mut file,0);
-            println!("metadata {:?}",metadata)
+            let metadata = get_schema(&mut file, 0);
+            println!("metadata {:?}", metadata);
         }
     }
 }
